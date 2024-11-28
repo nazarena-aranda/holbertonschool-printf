b7c63c0a902aced05c1af02c9095636ae6d201c9
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-28 14:51:37 +0000

    deletes comments innecesarys

--- printf.c
+++ printf.c
@@ -7,33 +7,34 @@
 int _printf(const char *format, ...)
 {
 	va_list args; /*declaramos la lista de argumentos*/
-	int i, count = 0; /*contar la cantidad de argumentos que nos pasen*/
+	int i, count = 0; 
 
-	if (format == NULL) /*verificamos si format es null*/
+	if (format == NULL)
 		return (-1);
 	va_start(args, format); /*inicializamos la lista de argumentos*/
-	for (i = 0; format[i] != '\0'; i++) /*recorremos format*/
+	for (i = 0; format[i] != '\0'; i++) 
 	{
-		if (format[i] == '%') /*si encontramos un especificador*/
+		if (format[i] == '%') 
 		{
-			i++; /*nos movemos al siguiente argumento luego del especificador*/
+			i++;
+			/*Procesamos segun el tipo de especificador*/
 			switch (format[i])
 			{
 				case 'c':
 					count += print_char(va_arg(args, int));
 					break;
-				case 's': /*corroboramos si es de tipo string*/
+				case 's':
 					count += print_str(va_arg(args, char *));
 					break;
-				case '%': /*corroboramos si es de tipo %*/
+				case '%':
 					count += write(1, "%", 1);
 					break;
-					/*en los casos de abajo corroboramos que sea un num*/
+					
 				case 'd':
 				case 'i':
 					count += print_num(va_arg(args, int));
 					break;
-				default: /*en el caso de que no sea ninguno de los que queremos, error*/
+				default: 
 					return (-1);
 			}
 		}
